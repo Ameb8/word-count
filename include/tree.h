88b74aa36af05fd47c997adcbad9f2fabc3c41e7
Alex Buckley <alexmeb81@outlook.com>
2025-05-27 19:43:12 -0700

    Fixed bugs in tree structure and tested functionality

--- include/tree.h
+++ include/tree.h
@@ -6,7 +6,7 @@
 typedef struct Tree Tree;
 
 Tree* tree_create(int (*compare)(const void*, const void*));
-char tree_set(Tree* tree, void* key, size_t key_size, int (*set_val)(void**, size_t*));
+char tree_set(Tree* tree, const void* key, const size_t key_size, int (*set_val)(void**, size_t*));
 void tree_print(Tree* tree, void (*print)(const void*, const void*, const size_t, const size_t));
 //unsigned long long tree_size(Tree* tree);
 //unsigned long long tree_unique(Tree* tree);
--- src/main.c
+++ src/main.c
@@ -4,20 +4,30 @@
 
 
 void test_print(const void* key, const void* val, const size_t key_size, const size_t val_size) {
-    printf("%llu occurrences of '%s'\n", (const unsigned long long)val, (const char*)key);
+    printf("%llu occurrences of '%s'\n", *(unsigned long long*)val, (const char*)key);
 }
 
 int compare_str(const void* a, const void* b) {
     return strcmp((const char*)a, (const char*)b);
 }
 
-void set_word(void** val, size_t* val_size) {
-    if(!*val)
-        *val = 1;
-    else
-        *val += 1;
-    
-    val_size = size_of(*val);
+
+int set_word_count(void** val, size_t* val_size) {
+    if(*val == NULL) { // New word added to tree
+        // Allocate memory for word count
+        unsigned long long* count = malloc(sizeof(unsigned long long));
+        
+        if(!count) // Allocation failed
+            return 0;
+
+        *count = 1;
+        *val = count;
+        *val_size = sizeof(unsigned long long);
+    } else { // Word already exists, increment count
+        unsigned long long* count = (unsigned long long*)(*val);
+        (*count)++;
+    }
+    return 1;
 }
 
 void test_file() {
@@ -40,7 +50,7 @@ int main() {
 
     int n = sizeof(strings) / sizeof(strings[0]);
     for (int i = 0; i < n; ++i) {
-        tree_set(tree, strings[i], size_of(strings[i]), set_word);
+        tree_set(tree, strings[i], sizeof(strings[i]), set_word_count);
     }
 
     printf("Tree (in-order):\n");
